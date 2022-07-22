12b80a3b084dfb89c228f29d64cde8a5260b5148
Tim Benniks <tbenniks@gmail.com>
2022-07-22 15:18:02 +0200

    cleaner conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 import { defineNuxtConfig } from 'nuxt'
-import manifestJson from './lib/context/context-manifest.json';
-import type { ManifestV2 } from '@uniformdev/context';
 
 export default defineNuxtConfig({
     runtimeConfig: {
@@ -10,31 +8,10 @@ export default defineNuxtConfig({
         },
     },
     modules: [
-
         '@uniformdev/uniform-nuxt',
-
-
     ],
     uniform: {
         projectId: process.env.UNIFORM_PROJECT_ID,
         readOnlyApiKey: process.env.UNIFORM_API_KEY,
-        manifest: manifestJson as ManifestV2,
-        defaultConsent: true,
-    },
-    // vite: {
-    //     optimizeDeps: {
-    //         include: ['rfdc', 'retry', 'p-throttle', 'p-retry'],
-    //     },
-    // },
-    // build: {
-    //     transpile: [
-    //         '@uniformdev/uniform-nuxt',
-    //         '@uniformdev/canvas-vue',
-    //         '@uniformdev/context-vue',
-    //         '@uniformdev/canvas',
-    //         '@uniformdev/context',
-    //         // uncomment this for netlify build
-    //         // process.env.NODE_ENV === 'production' && 'contentful',
-    //     ],
-    // },
+    }
 })
